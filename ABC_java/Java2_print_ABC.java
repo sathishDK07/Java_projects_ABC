a5d79e871089bb29913fc1e1fbd7cb44552f4480
sathishdk07 <sathishdk07@gmail.com>
2024-10-29 17:49:11 +0530

    java files

---
+++ ABC_java/Java2_print_ABC.java
@@ -0,0 +1,7 @@
+class Newfile1 {
+			public static void main (String[] args){
+				System.out.println("Hi");
+                                System.out.println("lets start java");
+                       		System.out.println("today");
+			}
+		}
